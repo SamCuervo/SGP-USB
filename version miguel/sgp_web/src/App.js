7fca9c3f6a24cef1f2131ed37b73cb1f8b0a4b74
miguelRodriguez1 <50088587+miguelRodriguez1@users.noreply.github.com>
2020-10-02 15:37:22 -0500

    Home
    plantilla home

---
+++ version miguel/sgp_web/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from './components/Header'
+import Menu from './components/Menu'
+import Footer from './components/Footer'
+import Content from './components/Content'
+import Home from './pages/Home'
+export default function App() {
+  return (
+    <div>
+      <Home/>
+    </div>
+  )
+}
